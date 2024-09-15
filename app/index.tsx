97899425dae1f4aeecba367c8dd2478c7c0192ae
Sebastian Barragán <barragan.palestino.sebastian@gmail.com>
2024-09-15 16:36:30 -0600

    Nativewind CSS, theme configured

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { Text } from "react-native";
 import { Screen } from "../components/Screen";
 import { Dashboard } from "../components/Dashboard";
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,50 @@ module.exports = {
     "./app/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#059669",
+          DEFAULT: "#059669",
+          dark: "#34d399",
+        },
+        secondary: {
+          light: "#059669",
+          DEFAULT: "#059669",
+          dark: "#34d399",
+        },
+        accent: {
+          light: "#200596",
+          DEFAULT: "#200596",
+          dark: "#4934d3",
+        },
+        background: {
+          light: "#ffffff",
+          DEFAULT: "#ffffff",
+          dark: "#242222",
+        },
+        danger: {
+          light: "#b41c2b",
+          DEFAULT: "#b41c2b",
+          dark: "#b41c2b",
+        },
+        success: {
+          light: "#009f42",
+          DEFAULT: "#009f42",
+          dark: "#009f42",
+        },
+        warning: {
+          light: "#cc8800",
+          DEFAULT: "#cc8800",
+          dark: "#cc8800",
+        },
+        info: {
+          light: "#388cfa",
+          DEFAULT: "#388cfa",
+          dark: "#388cfa",
+        },
+      },
+    },
   },
   plugins: [],
 };
